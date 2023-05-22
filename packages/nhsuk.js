7836805197278b5571db84cd44ed70cf4fa65215
Adam Fenwick <afenwickdev@gmail.com>
2023-05-22 17:39:57 +0100

    Cleanup of assets

--- packages/nhsuk.js
+++ packages/nhsuk.js
@@ -2,14 +2,7 @@ import GOVUKFrontend from 'govuk-frontend/govuk/all'
 
 // Components
 import Button from './components/button/button'
-import CharacterCount from './components/character-count/character-count'
-import Checkboxes from './components/checkboxes/checkboxes'
-import Details from './components/details/details'
-import ErrorSummary from './components/error-summary/error-summary'
 import Header from './components/header/header'
-import Radios from './components/radios/radios'
-// import SkipLink from './components/skip-link/skip-link'
-import Tabs from './components/tabs/tabs'
 
 import './polyfills'
 
@@ -17,13 +10,6 @@ GOVUKFrontend.initAll()
 
 // Initialize components
 document.addEventListener('DOMContentLoaded', () => {
-  CharacterCount()
   Button()
-  Checkboxes()
-  Details()
-  ErrorSummary()
   Header()
-  Radios()
-  // SkipLink()
-  Tabs()
 })
